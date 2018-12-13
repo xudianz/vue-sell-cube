b82ab9b2947dbcba5bb06bb9503aa88286e154c4
xudianzhang <xudianzhang@geotmt.com>
2018-12-13 11:38:39 +0800

    头部详情，星级组件

---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <support-ico class="icon" :size="size" :type="seller.supports[0].type"></support-ico>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div v-if="seller.supports" class="support-count" @click="detailShow">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+  </div>
+</template>
+
+<script>
+import supportIco from 'components/support-ico/support-ico'
+
+export default {
+  props: {
+    seller: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data () {
+    return {
+      size: 1
+    }
+  },
+  methods: {
+    detailShow () {
+
+    }
+  },
+  components: {
+    supportIco
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/mixin'
+@import '~common/stylus/variable'
+
+.header
+    position: relative
+    overflow: hidden
+    color: #fff
+    background: $color-background-ss
+    .content-wrapper
+      position: relative
+      padding: 24px 12px 18px 24px
+      font-size: 0
+      .avatar
+        display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size $fontsize-medium
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            display: inline-block
+            vertical-align: top
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            background-size: 30px 18px
+            background-repeat: no-repeat
+          .name
+            margin-left: 6px
+            font-size: $fontsize-large
+            line-height: 18px
+            font-weight: bold
+
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: $fontsize-small
+        .support
+          .icon
+            margin-right: 4px
+          .text
+            line-height: 12px
+            font-size: $fontsize-small-s
+
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        text-align: center
+        .count
+          vertical-align: top
+          font-size: $fontsize-small-s
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height: 24px
+          font-size: $fontsize-small-s
+
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: $color-background-sss
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        line-height 28px
+        font-size: $fontsize-small-s
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: $fontsize-small-s
+        right: 12px
+        top: 8px
+        margin-left 4px
+
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+</style>
